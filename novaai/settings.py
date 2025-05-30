bcf7d95924c0661abc2ef95702ad2df6a6b448b2
Noureddine YAMOUNI <118435516+nyamouni@users.noreply.github.com>
2025-05-30 11:41:34 +0200

    Update settings.py

--- novaai/settings.py
+++ novaai/settings.py
@@ -123,3 +123,4 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'yamouninoureddine99@gmail.com'
 EMAIL_HOST_PASSWORD = 'drqleuwfsvkdpalw' 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
