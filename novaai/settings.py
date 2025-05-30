63d62c028c7c35fa056d42760c280b2477e7e2d0
Noureddine YAMOUNI <118435516+nyamouni@users.noreply.github.com>
2025-05-30 11:48:08 +0200

    Update settings.py

--- novaai/settings.py
+++ novaai/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-wup1_dl7m)topgxu+rt)f#o2mw4ra%cbl&8i17vzfb%n9xmvby
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -107,8 +107,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  
+    os.path.join(BASE_DIR, 'contactform/static'),
 ]
+
 
 
 
